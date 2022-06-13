dba62f0370667c73a5f1c406b7942f51c55ba432
Przemysław Mirowski <215825@edu.p.lodz.pl>
2022-06-13 21:30:08 +0200

    [UNI-188] Update lab 08

--- lab_08/Zad2.py
+++ lab_08/Zad2.py
@@ -1,29 +1,33 @@
+from numpy import ndarray, sin, arange, pi, meshgrid
+from matplotlib.pyplot import subplots, clf, contourf, contour, show
+from matplotlib.animation import FuncAnimation
+
 import matplotlib.pyplot as plt
 import matplotlib.animation
 import numpy as np
 
 
-def f(x, y, t):
-    return 0.5 * np.sin(x ** 3) + 0.25 * np.sin((y - t * 0.05) ** 2)
+def f(x: ndarray, y: ndarray, t: int) -> ndarray:
+    return 0.5 * sin(x ** 3) + 0.25 * sin((y - t * 0.05) ** 2)
 
 
-x = np.arange(-np.pi / 2, np.pi / 2, 0.1)
-y = np.arange(0, np.pi * 1.5, 0.01)
+x = arange(-pi / 2, pi / 2, 0.1)
+y = arange(0, pi * 1.5, 0.01)
 
-fig, ax = plt.subplots(figsize=(10, 7))
-ax.set_xlim(-np.pi / 2, np.pi / 2)
-ax.set_ylim(0, np.pi * 1.5)
-
-
-def animate(frame):
-    plt.clf()
-
-    z = [[f(xx, yy, frame) for xx in x] for yy in y]
-    xx, yy = np.meshgrid(x, y)
-
-    plt.contourf(xx, yy, z, levels=10)
-    plt.contour(xx, yy, z, levels=10, colors='black')
+fig, ax = subplots(figsize=(10, 7))
+ax.set_xlim(-pi / 2, pi / 2)
+ax.set_ylim(0, pi * 1.5)
 
 
-ani = matplotlib.animation.FuncAnimation(fig, animate, frames=80, interval=0.1, repeat=False)
-plt.show()
+def animate(frame: int) -> None:
+    clf()
+
+    z = [[f(x=xx, y=yy, t=frame) for xx in x] for yy in y]
+    xx, yy = meshgrid(x, y)
+
+    contourf(xx, yy, z, levels=10)
+    contour(xx, yy, z, levels=10, colors='black')
+
+
+anim = FuncAnimation(fig, animate, frames=100, interval=0.1, repeat=False)
+show()
